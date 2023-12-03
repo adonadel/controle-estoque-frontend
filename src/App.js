ed4bd4b84d11aaf325708b0938a801e83cffab74
Éric <nascimento3ric@gmail.com>
2023-12-03 16:33:39 -0300

    Feat: instalando Mui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
         <a
           className="App-link"
           href="https://reactjs.org"
